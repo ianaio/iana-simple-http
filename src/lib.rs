5329967be585384ff531536da112c23cf663790c
Jaroslaw Cichon <dev@iana.io>
2023-04-14 10:30:59 +0000

    first start

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+// src/lib.rs
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Hello {}
+
+impl Component for Hello {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Welcome to the world of IANA SYSTEMS - RSW"}</span> }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Hello>::new().mount_to_body();
+}
+
